6d3d3141fc6eec0bbd53fb3e5f361f2d950ebcfd
Crizzt <yuriy.evseenko@gmail.com>
2014-03-31 13:03:26 +0300

    Initial commit

---
+++ static/js/chat.js
@@ -0,0 +1,5 @@
+window.onload = function() {
+	$chatFrame = document.getElementById('chat_frame');
+	$privateFrame = document.getElementById('private_frame');
+	$contactsFrame = document.getElementById('contacts_frame');
+}
