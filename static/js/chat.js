46f4bdea7a4dea2cbf17e475daffc81227becc0f
Crizzt <yuriy.evseenko@gmail.com>
2014-04-11 14:24:03 +0300

    Chat

--- static/js/chat.js
+++ static/js/chat.js
@@ -3,13 +3,13 @@ $(window).load(init_chat);
 function init_chat() {
 	$chatFrame = $('#chat_frame');
 	$contactsFrame = $('#contacts_frame');
-	$privateFrame = document.getElementById('private_frame');
-	$sendBtn = document.getElementById('send_btn')
-	$privateBtn = document.getElementById('private_btn')
+	$privateFrame = $('#private_frame');
+	$sendBtn = $('#send_btn');
+	$privateBtn = $('#private_btn');
 
-	$sendBtn.onclick = send;
+	$sendBtn.on('click', send);
 }
 
 function send() {
-	$privateFrame.innerHTML = 'Привет';
+	$privateFrame.html = 'Привет';
 }
