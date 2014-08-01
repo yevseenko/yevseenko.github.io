7deb349717d4070f752b97ecb248db0a54499a45
Crizzt <yuriy.evseenko@gmail.com>
2014-08-01 16:09:00 +0300

    spiders

--- static/js/spiders.js
+++ static/js/spiders.js
@@ -12,7 +12,7 @@ var $td = "<td></td>";
 
 
 
-var $spiders = [];
+var $spiders = new Array();
 add('Brachypelma Albopilosum','Female','Yes','13','19.05.2014','3');
 
 
@@ -25,11 +25,7 @@ add('Brachypelma Albopilosum','Female','Yes','13','19.05.2014','3');
 
 function add(kind, gender, venom, moult, lastMoult, period) {
 	var j = $spiders.length;
-	if (j === undefined) {
-		j = 0;
-	} else {
-		j = $spiders.length
-	}
+	$spiders[j] = new Object();
 	$spiders[j].kind = kind;
 	$spiders[j].gender = gender;
 	$spiders[j].venom = venom;
