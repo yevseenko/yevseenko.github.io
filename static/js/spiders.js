ad8142f69ed305afaded87eb822361595f87d948
Crizzt <yuriy.evseenko@gmail.com>
2014-08-01 16:21:00 +0300

    spiders

--- static/js/spiders.js
+++ static/js/spiders.js
@@ -16,6 +16,7 @@ var $td = "<td></td>";
 
 var $spiders = new Array();
 
+add('Вид','Пол','Яд','Линька','Дата последней линьки','Период между линьками');
 add('Brachypelma Albopilosum','Female','Yes','13','19.05.2014','3');
 add('Murinus','Male','Yes','8','28.06.2014','5');
 
