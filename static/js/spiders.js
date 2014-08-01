4a8a736726d969b27cab5e4c88cbd8dc473caa0b
Crizzt <yuriy.evseenko@gmail.com>
2014-08-01 17:36:23 +0300

    spiders

--- static/js/spiders.js
+++ static/js/spiders.js
@@ -3,9 +3,9 @@ $(document).ready(function() {
 	for(var i = 0; i < $spiders.length; i++) {
 		$('<tr><td>'+i+'</td><td>'+$spiders[i].kind+'</td><td>'+$spiders[i].gender+'</td><td>'+$spiders[i].venom+'</td><td>'+$spiders[i].moult+'</td><td>'+$spiders[i].lastMoult+'</td><td>'+$spiders[i].period+'</td></tr>').appendTo('table#spiders');
 	};
-	$('td:contains(Есть, очень опасен)').addClass('danger');
-	$('td:contains(Есть, опасен)').addClass('warning');
-	$('td:contains(Есть, не опасен)').addClass('info');
+	$('tr:contains(Есть, очень опасен)').addClass('danger');
+	$('tr:contains(Есть, опасен)').addClass('warning');
+	$('tr:contains(Есть, не опасен)').addClass('info');
 });
 
 var $spiders = new Array();
