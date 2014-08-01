52b7bf4ba120670b8c76adbb626404376eb92082
Crizzt <yuriy.evseenko@gmail.com>
2014-08-01 17:34:12 +0300

    spiders

--- static/js/spiders.js
+++ static/js/spiders.js
@@ -4,7 +4,8 @@ $(document).ready(function() {
 		$('<tr><td>'+i+'</td><td>'+$spiders[i].kind+'</td><td>'+$spiders[i].gender+'</td><td>'+$spiders[i].venom+'</td><td>'+$spiders[i].moult+'</td><td>'+$spiders[i].lastMoult+'</td><td>'+$spiders[i].period+'</td></tr>').appendTo('table#spiders');
 	};
 	$('td:contains(Есть, очень опасен)').addClass('danger');
-	add('Nhandu Coloratovilosus', 'Female', 'Есть, не опасен', 'L7 -> L8', '30.07.2014', '7 мес.');
+	$('td:contains(Есть, опасен)'),addClass('warning');
+	$('td:contains(Есть, не опасен)').addClass('info');
 });
 
 var $spiders = new Array();
