38def6bde5ae9bffb8b279b73693d303e8ea512e
Crizzt <yuriy.evseenko@gmail.com>
2014-08-01 19:54:49 +0300

    spiders

--- static/js/spiders.js
+++ static/js/spiders.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
 	$('tr:contains(Есть, очень опасен)').addClass('danger');
 	$('tr:contains(Есть, опасен)').addClass('warning');
 	$('tr:contains(Есть, не опасен)').addClass('success');
-	$('tr').sortable();
+	$('td').sortable();
 });
 
 var $spiders = new Array();
