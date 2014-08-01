d7b4892aa8e71bf5fa25bb791330e3e41eea9af4
Crizzt <yuriy.evseenko@gmail.com>
2014-08-01 15:30:55 +0300

    spiders

--- static/js/spiders.js
+++ static/js/spiders.js
@@ -1,7 +1,27 @@
 $(document).ready(function() {
+	$('#spiders').addClass('table table-bordered').width('95%');
 	$('<tr><td>' + $alba.kind + '</td></tr>').appendTo('table#spiders');
-	$('#spiders').addClass('table table-bordered').width('90%');
 });
+
+var $tr = "<tr></tr>";
+var $td = "<td></td>";
+
+
+
+
+
+
+
+
+var $alba = new spider('Brachypelma Albopilosum','Female','Yes','13','19.05.2014','3');
+
+
+
+
+
+
+
+
 
 function spider(kind, gender, venom, moult, lastMoult, period) {
 	this.kind = kind;
@@ -11,5 +31,3 @@ function spider(kind, gender, venom, moult, lastMoult, period) {
 	this.lastMoult = lastMoult;
 	this.period = period;
 };
-
-var $alba = new spider('Brachypelma Albopilosum','Female','Yes','13','19.05.2014','3');
