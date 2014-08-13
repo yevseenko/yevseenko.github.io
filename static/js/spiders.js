df545496ad2b8f7b7e5e013506dff7c452707720
Crizzt <yuriy.evseenko@gmail.com>
2014-08-13 19:02:17 +0300

    spiders

--- static/js/spiders.js
+++ static/js/spiders.js
@@ -35,13 +35,13 @@ $(document).ready(function() {
 var $spiders = new Array();
 
 add('Вид','Тип','Характер','Пол','Яд','Max. размер','Линька','Дата L ->','Период L');
-add('Brachypelma Albopilosum', 'Наземный', 'Спокойный', 'Female', 'Не опасен', 'До 16см.', 'L12 -> L13', '28.05.2014', '1 год.');
-add('Poecilotheria Ornata', 'Древесный', 'Агрессивный', 'Male', 'Очень опасен', 'До 24см.', 'L7 -> L8');
-add('Pterinochilus Murinus RCF', 'Древесный', 'Агрессивный', 'Female', 'Очень опасен', 'До 18см.', 'L7 -> L8', '8.08.2014', '2 мес.');
-add('Pterinochilus Murinus RCF', 'Древесный', 'Агрессивный', 'Не определен', 'Очень опасен','До 18см.', 'L7 -> L8', '19.07.2014', '3 мес.');
-add('Nhandu Coloratovillosus', 'Наземный', 'Нервный', 'Female', 'Не опасен', 'До 20см.', 'L7 -> L8', '30.07.2014', '4 мес.');
-add('Nhandu Coloratovillosus', 'Наземный', 'Нервный', 'Male', 'Не опасен', 'До 20см.', 'L5 -> L6');
-add('Poecilotheria Miranda', 'Древесный', 'Спокойный', 'Не определен', 'Опасен', 'До 21см.', 'L2 -> L3');
+add('Brachypelma Albopilosum','Наземный','Спокойный','Female','Не опасен','До 16см.','L12 -> L13','28.05.2014','1 год.');
+add('Poecilotheria Ornata','Древесный','Агрессивный','Male','Очень опасен','До 24см.','L7 -> L8');
+add('Pterinochilus Murinus RCF','Древесный','Агрессивный','Female','Очень опасен','До 18см.','L7 -> L8','8.08.2014','2 мес.');
+add('Pterinochilus Murinus RCF','Древесный','Агрессивный','Не определен','Очень опасен','До 18см.','L7 -> L8','19.07.2014','3 мес.');
+add('Nhandu Coloratovillosus','Наземный','Нервный','Female','Не опасен','До 20см.','L7 -> L8','30.07.2014','4 мес.');
+add('Nhandu Coloratovillosus','Наземный','Нервный','Male','Не опасен','До 20см.','L5 -> L6');
+add('Poecilotheria Miranda','Древесный','Спокойный','Не определен','Опасен','До 21см.','L2 -> L3');
 add('','Древесный','Спокойный','Не определен','Не опасен','','','','');
 
 function add(kind, types, character, gender, venom, sizes, moult, lastMoult, period) {
@@ -49,19 +49,19 @@ function add(kind, types, character, gender, venom, sizes, moult, lastMoult, per
 	$spiders[j] = new Object();
 	$spiders[j].kind = kind;
 	if (!$spiders[j].kind) {
-		$spiders[j].kind = '-';
+		$spiders[j].kind = 'Вид неизвестен';
 	};
 	$spiders[j].types = types;
 	if (!$spiders[j].types) {
-		$spiders[j].types = '-';
+		$spiders[j].types = 'Тип не определен';
 	};
 	$spiders[j].character = character;
 	if (!$spiders[j].character) {
-		$spiders[j].character = '-';
+		$spiders[j].character = 'Неизвестен';
 	};
 	$spiders[j].gender = gender;
 	if (!$spiders[j].gender) {
-		$spiders[j].gender = '-';
+		$spiders[j].gender = 'Не определен';
 	};
 	$spiders[j].venom = venom;
 	if (!$spiders[j].venom) {
