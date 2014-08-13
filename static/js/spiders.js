9ef6a599c7a83c37e4d2dc65bd361e5b7da79049
Crizzt <yuriy.evseenko@gmail.com>
2014-08-13 15:53:13 +0300

    spiders

--- static/js/spiders.js
+++ static/js/spiders.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 	$('#spiders').addClass('table table-bordered');
 	for(var i = 0; i < $spiders.length; i++) {
-		$('<tr><td>'+i+'</td><td>'+$spiders[i].kind+'</td><td>'+$spiders[i].gender+'</td><td>'+$spiders[i].venom+'</td><td>'+$spiders[i].moult+'</td><td>'+$spiders[i].lastMoult+'</td><td>'+$spiders[i].period+'</td></tr>').appendTo('table#spiders');
+		$('<tr><td>'+i+'</td><td>'+$spiders[i].kind+'</td><td>'+$spiders[i].types+'</td><td>'+$spiders[i].character+'</td><td>'+$spiders[i].gender+'</td><td>'+$spiders[i].venom+'</td><td>'+$spiders[i].moult+'</td><td>'+$spiders[i].lastMoult+'</td><td>'+$spiders[i].period+'</td></tr>').appendTo('table#spiders');
 	};
 	$('tr:contains(Есть, очень опасен)').addClass('danger');
 	$('tr:contains(Есть, опасен)').addClass('warning');
@@ -30,19 +30,21 @@ $(document).ready(function() {
 
 var $spiders = new Array();
 
-add('Вид','Пол','Яд','Линька','Дата последней линьки','Период между линьками');
-add('Brachypelma Albopilosum', 'Female', 'Есть, не опасен', 'L12 -> L13', '28.05.2014', '1 год.');
-add('Poecilotheria Ornata', 'Male', 'Есть, очень опасен', 'L7 -> L8');
-add('Pterinochilus Murinus RCF', 'Female', 'Есть, очень опасен', 'L7 -> L8', '8.08.2014', '2 мес.');
-add('Pterinochilus Murinus RCF', 'Не определен', 'Есть, очень опасен', 'L7 -> L8', '19.07.2014', '3 мес.');
-add('Nhandu Coloratovillosus', 'Female', 'Есть, не опасен', 'L7 -> L8', '30.07.2014', '4 мес.');
-add('Nhandu Coloratovillosus', 'Male', 'Есть, не опасен', 'L5 -> L6');
-add('Poecilotheria Miranda', 'Не определен', 'Есть, опасен', 'L2 -> L3');
+add('Вид','Тип','Характер','Пол','Яд','Линька','Дата последней линьки','Период между линьками');
+add('Brachypelma Albopilosum', 'Наземный', 'Спокойный', 'Female', 'Есть, не опасен', 'L12 -> L13', '28.05.2014', '1 год.');
+add('Poecilotheria Ornata', 'Древесный', 'Агрессивный', 'Male', 'Есть, очень опасен', 'L7 -> L8');
+add('Pterinochilus Murinus RCF', 'Древесный', 'Агрессивный', 'Female', 'Есть, очень опасен', 'L7 -> L8', '8.08.2014', '2 мес.');
+add('Pterinochilus Murinus RCF', 'Древесный', 'Агрессивный', 'Не определен', 'Есть, очень опасен', 'L7 -> L8', '19.07.2014', '3 мес.');
+add('Nhandu Coloratovillosus', 'Наземный', 'Нервный', 'Female', 'Есть, не опасен', 'L7 -> L8', '30.07.2014', '4 мес.');
+add('Nhandu Coloratovillosus', 'Наземный', 'Нервный', 'Male', 'Есть, не опасен', 'L5 -> L6');
+add('Poecilotheria Miranda', 'Древесный', 'Агрессивный', 'Не определен', 'Есть, опасен', 'L2 -> L3');
 
-function add(kind, gender, venom, moult, lastMoult, period) {
+function add(kind, types, character, gender, venom, moult, lastMoult, period) {
 	var j = $spiders.length;
 	$spiders[j] = new Object();
 	$spiders[j].kind = kind;
+	$spiders[j].types = types;
+	$spiders[j].character = character;
 	$spiders[j].gender = gender;
 	$spiders[j].venom = venom;
 	$spiders[j].moult = moult;
