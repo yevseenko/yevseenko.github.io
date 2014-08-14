8299b73da64c7d8a0935ddeb78d2c67c7c0859af
Crizzt <yuriy.evseenko@gmail.com>
2014-08-14 17:57:03 +0300

    spiders

--- static/js/spiders.js
+++ static/js/spiders.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 	for(var i = 0; i < $spiders.length; i++) {
-		$('<tr><td>'+i+'</td><td class="col-xs-2"><img src="../static/img/poecilotheria_miranda.jpg" height="10%" width="10%"></img>'+$spiders[i].kind+'</td><td>'+$spiders[i].types+'</td><td>'+$spiders[i].character+'</td><td>'+$spiders[i].gender+'</td><td>'+$spiders[i].venom+'</td><td>'+$spiders[i].sizes+'</td><td>'+$spiders[i].moult+'</td><td>'+$spiders[i].lastMoult+'</td><td>'+$spiders[i].period+'</td></tr>').appendTo('table#spiders');
+		$('<tr><td>'+i+'</td><td class="col-xs-3"><img src="../static/img/poecilotheria_miranda.jpg" height="10%" width="10%"></img>'+$spiders[i].kind+'</td><td>'+$spiders[i].types+'</td><td>'+$spiders[i].character+'</td><td>'+$spiders[i].gender+'</td><td>'+$spiders[i].venom+'</td><td>'+$spiders[i].sizes+'</td><td>'+$spiders[i].moult+'</td><td>'+$spiders[i].lastMoult+'</td><td>'+$spiders[i].period+'</td></tr>').appendTo('table#spiders');
 	};
 	$('tr:contains("Очень опасен")').addClass('danger');
 	$('tr:contains("Опасен")').addClass('warning');
