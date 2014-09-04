e8b9c034e81ac9898d7b40e3a6d59eb985412564
Crizzt <yuriy.evseenko@gmail.com>
2014-09-04 13:14:02 +0300

    godville

--- static/js/godville.js
+++ static/js/godville.js
@@ -2,9 +2,9 @@ $(document).ready(function() {
 
 	var $martial_glases = $('#martial_glases');
 
-	$martial_glases.text(martial_glases[martial_rand()]);
+	$martial_glases.val(martial_glases[martial_rand()]);
 	$martial_glases.on('click', function() {
-		$martial_glases.text(martial_glases[martial_rand()]);
+		$martial_glases.val(martial_glases[martial_rand()]);
 	});
 	$martial_glases.zclip({
 		path:"js/ZeroClipboard.swf",
