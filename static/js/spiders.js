9955dc920d318f397a9b3f91e1e0912cd03597cd
Crizzt <yuriy.evseenko@gmail.com>
2014-09-29 09:43:35 +0300

    some changes

--- static/js/spiders.js
+++ static/js/spiders.js
@@ -55,7 +55,7 @@ add('Poecilotheria Ornata','Древесный','Агрессивный','Male',
 add('Pterinochilus Murinus RCF','Древесный','Агрессивный','Female','Очень опасен','До 15см.','L7 -> L8','8.08.2014','2 мес.','../static/img/pterinochilus_murinus_rcf.jpg');
 add('Pterinochilus Murinus RCF','Древесный','Агрессивный','Не определен','Очень опасен','До 15см.','L7 -> L8','19.07.2014','3 мес.','../static/img/pterinochilus_murinus_rcf.jpg');
 add('Nhandu Coloratovillosus','Наземный','Нервный','Female','Не опасен','До 20см.','L7 -> L8','30.07.2014','4 мес.','../static/img/nhandu_coloratovillosus.jpg');
-add('Nhandu Coloratovillosus','Наземный','Нервный','Male','Не опасен','До 20см.','L5 -> L6','','','../static/img/nhandu_coloratovillosus.jpg');
+add('Nhandu Coloratovillosus','Наземный','Нервный','Male','Не опасен','До 20см.','L6 -> L7','28.09.2014','6 мес.','../static/img/nhandu_coloratovillosus.jpg');
 add('Lampropelma sp. Borneo Black','Норной','Агрессивный','Female','Очень опасен','До 18см.','L5','','','../static/img/lampropelma_sp_borneo_black.jpg');
 add('Poecilotheria Miranda','Древесный','Спокойный','Не определен','Опасен','До 21см.','L3 -> L4','24.09.2014','3 мес.','../static/img/poecilotheria_miranda.jpg');
 add('Davus Fasciatus','Норной','Нервный','Не определен','Не опасен','До 12см.','L1','','','../static/img/davus_fasciatus.jpg');
