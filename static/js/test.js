868a9a582259060ab79a765d58b18dfb7962adbb
Crizzt <yuriy.evseenko@gmail.com>
2016-11-29 16:23:45 +0200

    test script

--- static/js/test.js
+++ static/js/test.js
@@ -11,13 +11,32 @@ function sequence(start,step){
   };
 };
 
+
 function take(gen,x){
   var arr = [];
   for (var i = 0; i < x; i++) {
     arr.push(gen());
-  }
+  };
   return arr;
+};
+
+
+function map(fn,arr) {
+  var newArr = [];
+  for (var i = 0; i < arr.length; i++){
+    newArr.push(fn(arr[i]));
+  }
+  return newArr + " " + arr;
+}
+function square(x){
+  return x * x;
 }
 
+function fmap(a,gen) {
+  
+}
+
+
+var arr = [1,2,3];
 var generator = sequence(0,2);
 var generator2 = sequence(7,1);
