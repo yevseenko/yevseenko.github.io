2c915f7aa4e4f7ae31ff198f666309670cc77624
crizzt <yuriy.evseenko@gmail.com>
2016-12-03 16:49:26 +0200

    clock

--- static/js/clock.js
+++ static/js/clock.js
@@ -1,16 +1,11 @@
 $(document).ready(function() {
 	$dclock = document.getElementById('dclock');
-	digitalWatch();
+	digitalClock();
 });
 
-function digitalWatch() {
-	var date = new Date();
-    var hours = date.getHours();
-    var minutes = date.getMinutes();
-    var seconds = date.getSeconds();
-    if (hours < 10) hours = "0" + hours;
-    if (minutes < 10) minutes = "0" + minutes;
-    if (seconds < 10) seconds = "0" + seconds;
-    $dclock.innerHTML = hours + ":" + minutes + ":" + seconds;
-    setTimeout("digitalWatch()", 1000);
+function digitalClock() {
+	var date = new Date(),
+   	currentDate = date.toUTCString();
+    $dclock.innerHTML = currentDate;
+    setTimeout("digitalClock()", 1000);
 }
