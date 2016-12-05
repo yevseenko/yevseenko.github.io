bf75dd0976d318841c57f1e3e33bf7c51d414824
Crizzt <yuriy.evseenko@gmail.com>
2016-12-05 18:43:28 +0200

    Legendary insigt script

--- static/js/li.js
+++ static/js/li.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
 	$setCountLiBtn.onclick = liResult;
 });
 
-var arr = ["[&Agr2LQEA]","[&AgL2LQEA]","[&AgP2LQEA]","[&AgT2LQEA]","[&AgX2LQEA]","[&Agb2LQEA]","[&Agf2LQEA]","[&Agj2LQEA]","[&Agn2LQEA]","[&Agr2LQEA]","[&Agv2LQEA]","[&Agz2LQEA]","[&Ag32LQEA]","[&Ag72LQEA]","[&Ag/2LQEA]","[&AhD2LQEA]","[&AhH2LQEA]","[&AhL2LQEA]","[&AhP2LQEA]","[&AhT2LQEA]","[&AhX2LQEA]","[&Ahb2LQEA]","[&Ahf2LQEA]","[&Ahj2LQEA]","[&Ahn2LQEA]","[&Ahr2LQEA]","[&Ahv2LQEA]","[&Ahz2LQEA]","[&Ah32LQEA]","[&Ah72LQEA]","[&Ah/2LQEA]","[&AiD2LQEA]","[&AiH2LQEA]","[&AiL2LQEA]","[&AiP2LQEA]","[&AiT2LQEA]"];
+var arr = ["[&Agr2LQEA]","[&AgL2LQEA]","[&AgP2LQEA]","[&AgT2LQEA]","[&AgX2LQEA]","[&Agb2LQEA]","[&Agf2LQEA]","[&Agj2LQEA]","[&Agn2LQEA]","[&Agr2LQEA]","[&Agv2LQEA]","[&Agz2LQEA]","[&Ag32LQEA]","[&Ag72LQEA]","[&Ag/2LQEA]","[&AhD2LQEA]","[&AhH2LQEA]","[&AhL2LQEA]","[&AhP2LQEA]","[&AhT2LQEA]","[&AhX2LQEA]","[&Ahb2LQEA]","[&Ahf2LQEA]","[&Ahj2LQEA]","[&Ahn2LQEA]","[&Ahr2LQEA]","[&Ahv2LQEA]","[&Ahz2LQEA]","[&Ah32LQEA]","[&Ah72LQEA]","[&Ah/2LQEA]","[&AiD2LQEA]","[&AiH2LQEA]","[&AiL2LQEA]","[&AiP2LQEA]","[&AiT2LQEA]","[37] - значение отсутствует"];
 
 function liResult() {
 	if (!parseString($setCountLi).length) {
@@ -16,7 +16,7 @@ function liResult() {
 
 	var n = parseString($setCountLi) - 1;
 	if (n >= arr.length) {
-		$result.innerHTML = '<span class="glyphicon glyphicon-bold" aria-hidden="true"> - Banana!';
+		$result.innerHTML = '<span class="glyphicon glyphicon-bold" aria-hidden="true"> - Banana тебе, а не ' + parseString($setCountLi) + ' инсайтов';
 	} else {
 		$result.innerHTML = arr[n];
 	};
