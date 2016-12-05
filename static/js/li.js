5aff3da2d0fcfa0fc2aae9dccc01628b65f15b9f
Crizzt <yuriy.evseenko@gmail.com>
2016-12-05 19:44:20 +0200

    Legendary insigt script

--- static/js/li.js
+++ static/js/li.js
@@ -9,14 +9,14 @@ $(document).ready(function() {
 var arr = ["[&Agr2LQEA]","[&AgL2LQEA]","[&AgP2LQEA]","[&AgT2LQEA]","[&AgX2LQEA]","[&Agb2LQEA]","[&Agf2LQEA]","[&Agj2LQEA]","[&Agn2LQEA]","[&Agr2LQEA]","[&Agv2LQEA]","[&Agz2LQEA]","[&Ag32LQEA]","[&Ag72LQEA]","[&Ag/2LQEA]","[&AhD2LQEA]","[&AhH2LQEA]","[&AhL2LQEA]","[&AhP2LQEA]","[&AhT2LQEA]","[&AhX2LQEA]","[&Ahb2LQEA]","[&Ahf2LQEA]","[&Ahj2LQEA]","[&Ahn2LQEA]","[&Ahr2LQEA]","[&Ahv2LQEA]","[&Ahz2LQEA]","[&Ah32LQEA]","[&Ah72LQEA]","[&Ah/2LQEA]","[&AiD2LQEA]","[&AiH2LQEA]","[&AiL2LQEA]","[&AiP2LQEA]","[&AiT2LQEA]","[37] - значение отсутствует"];
 
 function liResult() {
-	if (!parseString($setCountLi).length) {
-		$result.innerHTML = 'Задайте значение';
+	if (!parseString($setCountLi).length || parseString($setCountLi) == 0) {
+		$result.innerHTML = 'Ничего так ничего';
 		return;
-	}
+	};
 
 	var n = parseString($setCountLi) - 1;
 	if (n >= arr.length) {
-		$result.innerHTML = '<b>B</b> - Banana тебе, а не ' + parseString($setCountLi) + ' инсайтов';
+		$result.innerHTML = '<b>B</b> - Banana тебе, а не <b>' + parseString($setCountLi) + '</b> инсайтов!';
 	} else {
 		$result.innerHTML = arr[n];
 	};
@@ -24,5 +24,9 @@ function liResult() {
 
 function parseString(elem) {
 	elem.value = elem.value.replace(/^\s+|\s+$/g, '');
-	return elem.value;
+	if (elem.value.length > 2) {
+		$result.innerHTML = 'О числе <b>' + elem.value + '</b> даже думать не хорошо. <b>Report</b>!';
+	} else {
+		return elem.value;
+	};
 };
