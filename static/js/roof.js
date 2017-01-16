2fb4578691d1f21d606ed77ce5dfe7edd02f3f88
Crizzt <yuriy.evseenko@gmail.com>
2017-01-16 11:00:42 +0200

    js com

--- static/js/roof.js
+++ static/js/roof.js
@@ -1,14 +1,12 @@
 $(document).ready(function() {
     var $div = $('.item');
 
-    for (var i = 0; i < $div.length; i++ ) {
-    	console.log(total($div[i]) + 'Element number:' + i);
-    }
-});
-
-function total(arr) {
+    var totalSum = $div.map(function(arr) {
 		var tmpArr = arr.innerHTML;
 		tmpArr = tmpArr.split('ть: ');
 		tmpArr = tmpArr[1].split(' ');
 		return tmpArr[0];
-}
+	});
+    console.log(totalSum);
+});
+
