5b129fa6bf648aa03c43878f791b20f0431e778d
Crizzt <yuriy.evseenko@gmail.com>
2017-01-16 12:20:21 +0200

    js com

--- static/js/roof.js
+++ static/js/roof.js
@@ -1,19 +1,30 @@
 $(document).ready(function() {
     var $div = $('.item');
     var	$totalNeed = $('#totalNeed');
+    var	$totalSum = $('#totalSum');
 
     for (var i = 0; i < $div.length; i++) {
     	getArr($div[i]);
+    	getArrSum($div[i]);
     }
 
-    $totalNeed[0].innerHTML = 'Еще необходимо сдать: ' + sumArr.reduce(function(sum, current){ return sum + current }, 0);
+    $totalSum[0].innerHTML = 'Уже сдано: ' + totalArr.reduce(function(sum, current){ return sum + current }, 0) + '₴ (гривен)';
+    $totalNeed[0].innerHTML = 'Еще необходимо сдать: ' + totalArr.reduce(function(sum, current){ return sum + current }, 0) + '₴ (гривен)';
 });
 
+var totalArr = [];
 var sumArr = [];
 
 function getArr(elem) {
 	var tmpArr = elem.innerHTML;
 	tmpArr = tmpArr.split('ть: ');
+	tmpArr = tmpArr[1].split(' ');
+	totalArr.push(parseInt(tmpArr[0]));
+}
+
+function getArrSum(elem) {
+	var tmpArr = elem.innerHTML;
+	tmpArr = tmpArr.split('но: ');
 	tmpArr = tmpArr[1].split(' ');
 	sumArr.push(parseInt(tmpArr[0]));
 }
