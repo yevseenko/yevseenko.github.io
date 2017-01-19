28130e151086fbc7e3ee2194ccaacefb47bc6f63
Crizzt <yuriy.evseenko@gmail.com>
2017-01-19 10:40:19 +0200

    roof floor

--- static/js/roof.js
+++ static/js/roof.js
@@ -35,3 +35,26 @@ function getArrSum(elem) {
 	}
 	sumArr.push(parseInt(tmpArr[0]));
 }
+
+var values = [ 
+	{category: 1, val1: 2, val2: 3},
+	{category: 1, val1: 5, val2: 4},
+	{category: 1, val1: 7, val2: 7},
+	{category: 2, val1: 21, val2: 21}, 
+	{category: 2, val1: 42, val2: 23},
+	{category: 3, val1: 2, val2: 3} 
+] 
+
+var totalSum = {val1:0, val2:0}; 
+var categoriesSum = {}; 
+
+values.forEach(function(v){ 
+	var categorySum = categoriesSum[v.category] || (categoriesSum[v.category] = {val1:0, val2:0}); 
+	categorySum.val1 += v.val1; 
+	categorySum.val2 += v.val2; 
+	totalSum.val1 += v.val1; 
+	totalSum.val2 += v.val2; 
+	}); 
+
+console.log(totalSum); 
+console.log(categoriesSum);
