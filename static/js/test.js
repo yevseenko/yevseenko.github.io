dca44ffe9eadb1c64b9ad9cd9a273c0efc144ee5
Crizzt <yuriy.evseenko@gmail.com>
2017-01-25 20:16:44 +0200

    test page

--- static/js/test.js
+++ static/js/test.js
@@ -1,35 +1,7 @@
-$(document).ready(function() {
-    $box = document.getElementById('box');
-    $reset = document.getElementById('resetCountBtn');
-    $next = document.getElementById('nextCountBtn');
-    $set = document.getElementById('setCountBtn');
-    $setCount = document.getElementById('setCount');
-    
-    $box.innerHTML = "Counter: " + counter();
-    $next.onclick = (function() {$box.innerHTML = "Counter: " + counter()});
-    $reset.onclick = (function() {$box.innerHTML = "Counter: " + counter.reset()});
-    $set.onclick = (function() {$box.innerHTML = "Counter: " + counter.set($setCount.value)});
-  });
+var arrays = [
+    [1, 2, 3],
+    [4, 5],
+    [6]
+];
 
-//примитивный счетчик
-function makeCounter(){
-  var currentCount = 1;
-  function counter() {
-    return currentCount++;
-  }
-  counter.set = function(value){
-    if (!value) {
-      return currentCount = "значение не задано";
-    } else {
-      return currentCount = value;
-    }
-  }
-  counter.reset = function() {
-    return currentCount = 1;
-  }
-  return counter;
-};
-
-var counter = makeCounter();
-
-/* Странный комментарий для теста */
+console.log(arrays.reduce(function (a,b) { return a.concat(b)}));
