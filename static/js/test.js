4ecf4d76c274fcb313827dca6c23ef8a680f7590
Crizzt <yuriy.evseenko@gmail.com>
2017-02-02 17:02:05 +0200

    test

--- static/js/test.js
+++ static/js/test.js
@@ -66,9 +66,9 @@ var result1 = 'Not used..',
 	result5 = 'Not used..';
 
 var arrays = [
-    [1, 2, 3],
-    [4, 5],
-    [6]
+	[1, 2, 3],
+	[4, 5],
+	[6]
 ];
 
 var ancestry = JSON.parse(ANCESTRY_FILE);
@@ -90,3 +90,17 @@ function havem(p) { return p.mother != null; }
 function rmother(p) { return byName[p.mother] != undefined; }
 
 result2 = average(ancestry.filter(havem).filter(rmother).map(childborn));
+
+function revrot(str, sz) {
+  if (sz < 1 || sz > str.length) 
+    return '';
+
+  let reverse = s => s.split('').reverse().join('');
+  let rotate  = s => s.slice(1) + s.slice(0, 1);
+  let sum_cubes = c => c.split('').reduce((a, b) => a + +b ** 3, 0); 
+
+  return str
+    .match(new RegExp('.{' + sz + '}', 'g'))
+    .map(c => sum_cubes(c) % 2 ? rotate(c) : reverse(c))
+    .join('');
+}
