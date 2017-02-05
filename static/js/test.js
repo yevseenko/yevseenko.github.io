d1c63a75a8b6038ec9b4133ebfe7225b8416696b
crizzt <yuriy.evseenko@gmail.com>
2017-02-05 22:32:36 +0200

    'test'

--- static/js/test.js
+++ static/js/test.js
@@ -1,21 +1,9 @@
 'use strict';
 
-function fastestIsPalindrome(str) {
-  var len = Math.floor(str.length / 2);
-  for (var i = 0; i < len; i++)
-    if (str[i] !== str[str.length - i - 1])
-      return false;
-  return true;
-}
-
-//Decorator
-function logArgs(f){
-  return function(){
-    console.log(arguments);
-    return f.apply(this, arguments);
+function bingo(ticket, win){
+  let result = 0;
+  for (var i = 0; i < ticket.length; i++) {
+    if (ticket[i][0].charCodeAt(i) == ticket[i][1]) result += 1;
   }
-}
-
-function sum(x,y) {
-  return x + y;
+  return result >= win ? 'Winner!' : 'Loooser!';
 }
