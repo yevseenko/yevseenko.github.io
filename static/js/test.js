3b47f0094fd6620fbcd4b24413cc6c728a83f264
Crizzt <yuriy.evseenko@gmail.com>
2017-02-06 21:02:33 +0200

    test

--- static/js/test.js
+++ static/js/test.js
@@ -2,7 +2,11 @@
 
 function longestConsec(strarr, k) {
 	if (strarr.length == 0 || k > strarr.length || k <= 0) return '';
-    return strarr.sort(function(a,b){return a.length < b.length}).slice(0,k).join('');
+	var arr = [];
+	for (var i = 0; i <= (strarr.length/k) + 1 ; i++) {
+		arr.push(strarr.slice(i,i+k).join(''));
+	}
+	return arr;
 }
 
 
