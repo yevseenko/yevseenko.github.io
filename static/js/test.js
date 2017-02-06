c56a943f02865a5f70eea265090b5e225224f4e3
crizzt <yuriy.evseenko@gmail.com>
2017-02-07 00:00:11 +0200

    'test'

--- static/js/test.js
+++ static/js/test.js
@@ -1,15 +1,9 @@
 'use strict';
 
-function longestConsec(strarr, k) {
-	if (strarr.length == 0 || k > strarr.length || k <= 0) return '';
-	var arr = [];
-	for (var i = 0; i <= (strarr.length/k) + 1 ; i++) {
-		arr.push(strarr.slice(i,i+k).join(''));
-	}
-	return arr;
+function list_squared{
+
 }
 
+// list_squared(1, 250) --> [[1, 1], [42, 2500], [246, 84100]]
 
-// longestConsec(["zone", "abigail", "theta", "form", "libe", "zas"], 2)
-
-// https://www.codewars.com/kata/consecutive-strings/train/javascript
+// https://www.codewars.com/kata/integers-recreation-one
