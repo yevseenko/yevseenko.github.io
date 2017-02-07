4adae6678d5f0df5a2f9bcef941802b6a10ddf14
crizzt <yuriy.evseenko@gmail.com>
2017-02-07 23:06:59 +0200

    'test'

--- static/js/test.js
+++ static/js/test.js
@@ -1,8 +1,23 @@
 'use strict';
 
-function digital_root(n) {
-  return (n - 1) % 9 + 1;
+function DNAStrand(dna){
+    //let arr = dna.split('');
+    /*for (let i = 0; i < arr.length; i++) {
+        if (arr[i] == 'A') {
+            arr[i] = 'T';
+        } else if (arr[i] == 'T') {
+            arr[i] = 'A';
+        } else if (arr[i] == 'C') {
+            arr[i] = 'G';
+        } else if (arr[i] == 'G') {
+            arr[i] = 'C';
+        }
+    }*/
+    return dna.split('').map(function(y){ return (y == 'A') ? 'T' : (y == 'T') ? 'A' : (y == 'C') ? 'G' : 'C'}).join('');
 }
+
 // https://www.codewars.com/kata/sum-of-digits-slash-digital-root/train/javascript
+
+// DNAStrand("ATTGC"),"TAACG","String ATTGC is" A -> T && C -> G
 
 // Math.exp(Math.log(81) / 2).toFixed(5)
