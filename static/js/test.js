c0cb164254d66416f1db974cd1831189ee4a093a
Crizzt <yuriy.evseenko@gmail.com>
2017-02-09 20:34:54 +0200

    test

--- static/js/test.js
+++ static/js/test.js
@@ -1,23 +1,25 @@
 'use strict';
 
-function DNAStrand(dna){
-    //let arr = dna.split('');
-    /*for (let i = 0; i < arr.length; i++) {
-        if (arr[i] == 'A') {
-            arr[i] = 'T';
-        } else if (arr[i] == 'T') {
-            arr[i] = 'A';
-        } else if (arr[i] == 'C') {
-            arr[i] = 'G';
-        } else if (arr[i] == 'G') {
-            arr[i] = 'C';
+function sequenceSum(begin, end, step) {
+    if (begin > end && step > 0) return 0;
+    if (begin < end && begin < 0 && end < 0 && step < 0) return 0;
+    var result = 0;
+    if (begin < 0 && end < 0 && step < 0) {
+        for (var i = begin; i >= end; i += step) {
+            result += i;
         }
-    }*/
-    return dna.split('').map(function(y){ return (y == 'A') ? 'T' : (y == 'T') ? 'A' : (y == 'C') ? 'G' : 'C'}).join('');
+    } else if (begin > 0 && (end < 0 || end > 0) && step < 0) {
+        for (var i = begin; i >= end; i += step) {
+            result += i;
+        }
+    } else {
+        result = (begin + (end - end%step))/2*((end-end%step-begin)/step + 1);        
+    }
+    return result;
 }
 
-// https://www.codewars.com/kata/sum-of-digits-slash-digital-root/train/javascript
+// https://www.codewars.com/kata/sum-of-a-sequence-hard-core-version/train/javascript
 
-// DNAStrand("ATTGC"),"TAACG","String ATTGC is" A -> T && C -> G
+// sequenceSum(-1, -5, -3), -5)
 
 // Math.exp(Math.log(81) / 2).toFixed(5)
