ec65e9617834bfc58d477a6f5026badee3e1f2ab
Crizzt <yuriy.evseenko@gmail.com>
2017-02-10 21:12:22 +0200

    test

--- static/js/test.js
+++ static/js/test.js
@@ -4,14 +4,7 @@ function sequenceSum(begin, end, step) {
     if (begin > end && step > 0) return 0;
     if (begin < end && begin < 0 && end < 0 && step < 0) return 0;
     var result = 0;
-    if (begin < 0 && end < 0 && step < 0) {
-        for (var i = begin; i >= end; i += step) {
-            result += i;
-        }
-    } else if (begin > 0 && (end < 0 || end > 0) && step < 0) {
-        for (var i = begin; i >= end; i += step) {
-            result += i;
-        }
+    
     } else {
         result = (begin + (end - end%step))/2*((end-end%step-begin)/step + 1);        
     }
