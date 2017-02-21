d5cb1c69d67951936ab8b3e425ff9d883d42ac36
Crizzt <yuriy.evseenko@gmail.com>
2017-02-21 20:33:44 +0200

    redux

--- static/redux/app.js
+++ static/redux/app.js
@@ -1,14 +1,16 @@
-var state = {};
+const state = {};
 
 state.todos = [];
 state.filters = [];
 state.selectedFilter;
+state.newTodoTitle;
 
-function render(state) {
-	document.body.innerHTML = `
-		<input type="text"></input>
+function renderToString(state) {
+	return `
+		<input type="text" value="${state.newTodoTitle || ''}"></input>
 
 		<ul>
+			${renderTodos(state.todos)}
 		</ul>
 
 		<ul>
@@ -16,3 +18,22 @@ function render(state) {
 
 	`;
 }
+
+function render(state) {
+	document.body.innerHTML = renderToString(state);
+}
+
+function renderTodos(todos) {
+	return todos.map( todo => {
+		return `<li>${todo.title}</li>`
+	})
+	.join('\n');
+}
+
+render(state);
+
+const input = document.querySelector('input');
+
+input.addEventListener('change', e => {
+	console.log(e);
+})
