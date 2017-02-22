ad81eae7ad91ec70e4448cf9816d72954249f1ed
Crizzt <yuriy.evseenko@gmail.com>
2017-02-22 15:01:47 +0200

    redux

--- static/redux/app.js
+++ static/redux/app.js
@@ -4,25 +4,25 @@ state.todos = [];
 state.newTodoTitle;
 
 class StateStore {
-	constructor(initialState = {}, reducer) {
-		this.state = initialState;
-		this.listeners = [];
-		this.reducer = reducer;
-	}
+    constructor(initialState = {}, reducer) {
+        this.state = initialState;
+        this.listeners = [];
+        this.reducer = reducer;
+    }
 
-	subscribe(listener) {
-		this.listeners.push(listener);
-	}
+    subscribe(listener) {
+        this.listeners.push(listener);
+    }
 
-	dispatch(action) {
-		this.state = this.reducer(this.state, action);
-		this.listeners.forEach( listener => listener(this.state));
-	}
+    dispatch(action) {
+        this.state = this.reducer(this.state, action);
+        this.listeners.forEach(listener => listener(this.state));
+    }
 }
 
 const stateStore = new StateStore({
-	todos: [],
-	newTodoTitle: ''
+    todos: [],
+    newTodoTitle: ''
 }, appReducer);
 
 stateStore.subscribe(render);
@@ -30,33 +30,33 @@ stateStore.subscribe(render);
 let todoId = 0;
 
 function appReducer(state, action) {
-	switch (action.type) {
-		case 'SET_NEW_TODO_TITLE_VALUE':
-			return Object.assign({}, state, {
-				newTodoTitle: action.payload
-			});
-		case 'ADD_TODO':
-			return Object.assign({}, state, {
-				todos: state.todos.concat([ Object.assign({ id: todoId++ }, action.payload ) ])
-			});
-		case 'COMPLETE_TODO':
-			return Object.assign({}, state, {
-				todos: state.todos.map( todo => {
-					if (action.payload === todo.id) {
-						todo.done = true;						
-					}
-					return todo;
-				})
-			});
-	}
+    switch (action.type) {
+        case 'SET_NEW_TODO_TITLE_VALUE':
+            return Object.assign({}, state, {
+                newTodoTitle: action.payload
+            });
+        case 'ADD_TODO':
+            return Object.assign({}, state, {
+                todos: state.todos.concat([Object.assign({ id: todoId++ }, action.payload)])
+            });
+        case 'COMPLETE_TODO':
+            return Object.assign({}, state, {
+                todos: state.todos.map(todo => {
+                    if (action.payload === todo.id) {
+                        todo.done = true;
+                    }
+                    return todo;
+                })
+            });
+    }
 }
 
 function render(state) {
-	document.body.innerHTML = renderToString(state);
+    document.body.innerHTML = renderToString(state);
 }
 
 function renderToString(state) {
-	return `
+    return `
 		<input type="text" value="${state.newTodoTitle || ''}"></input>
 
 		<ul>
@@ -70,8 +70,8 @@ function renderToString(state) {
 }
 
 function renderTodos(todos) {
-	return todos.map( todo => {
-		return `<li 
+    return todos.map(todo => {
+            return `<li 
 					class="${todo.done ? '-completed' : ''}todo-item"
 					data-id="${todo.id}"
 					>
@@ -80,47 +80,47 @@ function renderTodos(todos) {
 
 				</li>
 				`
-	})
-	.join('\n');
+        })
+        .join('\n');
 }
 
 render(state);
 
 document.body.addEventListener('keypress', e => {
-	const input = document.querySelector('input');
+    const input = document.querySelector('input');
 
-	if (e.keyCode === 13) {
-		stateStore.dispatch({
-		type: 'ADD_TODO',
-		payload: {
-			title: input.value,
-			done: false
-		}
-	});
+    if (e.keyCode === 13) {
+        stateStore.dispatch({
+            type: 'ADD_TODO',
+            payload: {
+                title: input.value,
+                done: false
+            }
+        });
 
-	stateStore.dispatch({
-		type: 'SET_NEW_TODO_TITLE_VALUE',
-		payload: ''
-	});
+        stateStore.dispatch({
+            type: 'SET_NEW_TODO_TITLE_VALUE',
+            payload: ''
+        });
 
-	return;
-	}
+        return;
+    }
 
-	stateStore.dispatch({
-		type: 'SET_NEW_TODO_TITLE_VALUE',
-		payload: input.value + e.key
-	});
+    stateStore.dispatch({
+        type: 'SET_NEW_TODO_TITLE_VALUE',
+        payload: input.value + e.key
+    });
 
-	makeMyLifeAwesome();
+    makeMyLifeAwesome();
 });
 
 document.body.addEventListener('click', e => {
-	if (e.target.classList.contains('todo-item')) {
-		stateStore.dispatch({
-			type: 'COMPLETE_TODO',
-			payload: +e.target.dataset.id
-		})
-	}
+    if (e.target.classList.contains('todo-item')) {
+        stateStore.dispatch({
+            type: 'COMPLETE_TODO',
+            payload: +e.target.dataset.id
+        })
+    }
 });
 
 
@@ -145,7 +145,7 @@ document.body.addEventListener('click', e => {
 
 
 function makeMyLifeAwesome() {
-	setTimeout(() => {
-		document.querySelector('input').focus();
-	}, 0);
+    setTimeout(() => {
+        document.querySelector('input').focus();
+    }, 0);
 }
