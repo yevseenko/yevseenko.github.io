6c241df6f1b66cde1ebf262e10c93d96842e0043
Crizzt <yuriy.evseenko@gmail.com>
2017-02-23 15:44:38 +0200

    test

--- static/js/test.js
+++ static/js/test.js
@@ -1,17 +1,32 @@
 'use strict';
 
-var list = document.querySelector('#list'),
-	input = document.querySelector('input');
+(function() {
+    var list = document.querySelector('ol'),
+        input = document.querySelector('input');
 
-list.addEventListener('click', function(e){
-	if (e.target.classList.contains('todo-item')) {
-		e.target.classList.toggle('completed');
-	}
-});
+    list.addEventListener('click', function(e) {
+        if (e.target.classList.contains('todo-item')) {
+            e.target.classList.toggle('completed');
+        }
+    });
 
-input.addEventListener('keypress', function(e) {
-	if (e.keyCode === 13 && input.value) {
-		list.innerHTML += '<li class="todo-item">' + input.value + '</li>';
-		input.value = '';
-	}
-});
+    input.addEventListener('keypress', function(e) {
+        if (e.keyCode === 13 && input.value) {
+            list.innerHTML += '<li class="todo-item">' + input.value + ' ' + dateString() + '</li>';
+            input.value = '';
+        }
+    });
+
+    function dateString() {
+        var date = new Date(),
+            seconds = date.getSeconds(),
+            minutes = date.getMinutes(),
+            hours = date.getHours();
+
+        return '[' + addZero(hours) + ':' + addZero(minutes) + ':' + addZero(seconds) + ']';
+    }
+
+    function addZero(n) {
+        return n < 10 ? n = "0" + n : n;
+    }
+}());
