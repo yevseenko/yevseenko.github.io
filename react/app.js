a37896992c96336b9601d928fa45b8e8976b816d
Crizzt <yuriy.evseenko@gmail.com>
2017-02-27 11:51:52 +0200

    react

--- react/app.js
+++ react/app.js
@@ -1,23 +1,136 @@
 var HomeListing = React.createClass({
+  /*One option is to pull data from, and save data to,
+    a json url for testing. On a larger scale,
+    this React output would be part of a larger app
+    where the data is saved in a database as part of
+    an MVC, where React is the View in the
+    Model-View-Controller
+  */
+  loadHomesFromServer: function() {
+    //We're going to hard-code this information for testing purposes.
+    var homes = [
+      {
+          "address": "12345 Beverly Dr",
+          "description": "This is a home in the city",
+          "photo": "assets/images/home.jpg",
+          "saves": 52,
+          "saved": false
+      },
+      {
+        "address": "98765 Tweety Ln",
+        "description": "This is a home in the suburbs",
+        "photo": "assets/images/home.jpg",
+        "saves": 123,
+        "saved": true
+      },
+      {
+        "address": "1 Small St.",
+        "description": "This is a nice little country home",
+        "photo": "assets/images/home.jpg",
+        "saves": 189,
+        "saved": false
+      }
+    ];
+    this.setState({homes: homes});
+  },
+  loadSavesFromServer: function() {
+    //Again, we hard-code for testing
+    var saves = [
+      {
+        "saves": 52,
+        "saved": false
+      },
+      {
+        "saves": 123,
+        "saved": true
+      },
+      {
+        "saves": 189,
+        "saved": false
+      }
+    ];
+    this.setState({saves: saves});
+  },
+  toggleSave: function(index) {
+    
+    var saves = this.state.saves;
+    
+  	if (saves[index].saved) {
+      saves[index].saves--;
+      saves[index].saved = false;
+		}
+		else {
+  		saves[index].saves++;
+      saves[index].saved = true;
+		}
+		this.setState({
+			saves: saves,
+		});
+    
+    //This is where we would save the information if this were part of a larger app
+    return saves[index].saved;
+ 
+	},
+	getInitialState: function(){
+ 
+		var saves = [];
+    var homes = [];
+    
+		return {
+      saves: saves,
+      homes: homes
+    }
+	},
+  componentDidMount: function() {
+    this.loadHomesFromServer();
+    this.loadSavesFromServer();
+    //If we were updating the saves, we could continuously poll the server,
+    //and update the Saves information when something changes
+    //setInterval(this.loadSavesFromServer, this.props.pollInterval);
+  },
   render: function() {
+    //We need to set these variables (including the toggleSave function)
+    //so they can be used within the map function below. Otherwise, they
+    //would be outside the function's scope
+    var saves = this.state.saves;
+    var toggleSave = this.toggleSave;
+    
+    var homeNodes = this.state.homes.map(function(home, index) {
+      
+      if (typeof(saves[index]) == "undefined") {
+        saves[index] = {saves: 0};
+      }
+      //the key is React-specific, and is especially important when
+      //components can be shuffled or removed. it is NOT available
+      //as a prop, so we need a separate id for that.
+      return (
+        <Home
+          key={index}
+          id={index}
+          onToggleSave={toggleSave}
+          isSaved={saves[index].saved}
+          photo={home.photo}
+          address={home.address}
+          numSaves={saves[index].saves}
+        >
+          {home.description}
+        </Home>
+      );
+    });
     return (
       <div className="homeList">
-        <Home
-          key={0}
-          id={0}
-          isSaved={false}
-          photo="assets/images/home.jpg"
-          address="12345 Beverly Dr"
-          numSaves={52}
-        >
-          This is a home in the city
-        </Home>
+        {homeNodes}
       </div>
     );
   }
 });
  
 var Home = React.createClass({
+  toggleSave: function(index){
+    //We have to do a second pass to the top level parent,
+    //since that is where the entire list resides.
+    return this.props.onToggleSave(index);
+  },
   render: function() {
     return (
       <div className="home">
@@ -30,6 +143,7 @@ var Home = React.createClass({
         </span>
         <Saves
           id={this.props.id}
+          handleSave={this.toggleSave}
           isSaved={this.props.isSaved}
           numSaves={this.props.numSaves}
         ></Saves>
@@ -38,40 +152,29 @@ var Home = React.createClass({
   }
 });
  
-var Photo = (props) => {
-  return (<div className="homePhoto">
-    <img src={props.src} />
-  </div>);
-};
+var Photo = React.createClass({
+  render: function() {
+    return (
+      <div className="homePhoto">
+        <img src={this.props.src} />
+      </div>
+    );
+  }
+});
  
 var Saves = React.createClass({
-  getInitialState: function(){
-    return {
-      saved: this.props.isSaved,
-      numSaves: this.props.numSaves
-    }
-  },
   handleSubmit: function(e) {
+    //We prevent the default action of submitting the form
+    //so we can stay on the page
     e.preventDefault();
- 
-    var saved = false;
-    var numSaves = this.state.numSaves;
     
-    if (this.state.saved === false) {
-      saved = true;
-      numSaves++;
-    } else {
-      numSaves--;
-    }
-    this.setState({
-      numSaves: numSaves,
-      saved: saved
-    });
+    //We have to pass this up to the parent
+    var isSaved = this.props.handleSave(this.props.id);
   },
   render: function() {
     var savedText = '';
     var submitText = 'Save';
-    if (this.state.saved) {
+    if (this.props.isSaved) {
       savedText = 'You have saved this home.';
       submitText = 'Remove';
     }
@@ -81,13 +184,13 @@ var Saves = React.createClass({
         <form onSubmit={this.handleSubmit}>
           <input type="submit" value={submitText} />
         </form>
-      {this.state.numSaves} saves. {savedText}
+      {this.props.numSaves} saves. {savedText}
       </div>
     );
   }
 });
  
-ReactDOM.render(
-  <HomeListing />,
+React.render(
+  <HomeListing url="homes.json" savesUrl="saves.json" pollInterval={10000} />,
   document.getElementById('root')
 );
