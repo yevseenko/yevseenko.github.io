5d407b8ea833f99d293f9e7d4afac743c99ef4af
Crizzt <yuriy.evseenko@gmail.com>
2017-02-27 11:02:06 +0200

    react

--- react/app.js
+++ react/app.js
@@ -2,4 +2,7 @@ var element = <div className="whatever">
    Some text
 </div>;
 
+const elem = <div className="hello">hello world</div>;
+
 ReactDOM.render(element, document.getElementById('root'));
+ReactDOM.render(elem, document.getElementById('root'));
