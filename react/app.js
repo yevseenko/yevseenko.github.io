533f1c6c0f236557d4f59d71e287c22efd178141
Crizzt <yuriy.evseenko@gmail.com>
2017-02-27 11:43:32 +0200

    react

--- react/app.js
+++ react/app.js
@@ -1,8 +1,44 @@
-var element = <div className="whatever">
-   Some text
-</div>;
+const elem = React.createClass({
+  getInitialState: function(){
+    return {
+      saved: false,
+      numSaves: 0
+    }
+  },
+  handleSubmit: function(e) {
+    e.preventDefault();
+ 
+    var saved = false;
+    var numSaves = this.state.numSaves;
+    
+    if (this.state.saved === false) {
+      saved = true;
+      numSaves++;
+    } else {
+      numSaves--;
+    }
+    this.setState({
+      numSaves: numSaves,
+      saved: saved
+    });
+  },
+  render: function() {
+    var savedText = '';
+    var submitText = 'Save';
+    if (this.state.saved) {
+      savedText = 'You have saved this home.';
+      submitText = 'Remove';
+    }
+    
+    return (
+      <div className="saves">
+        <form onSubmit={this.handleSubmit}>
+          <input type="submit" value={submitText} />
+        </form>
+      {this.state.numSaves} saves. {savedText}
+      </div>
+    );
+  }
+});
 
-const elem = <div className="hello">hello world</div>;
-
-ReactDOM.render(element, document.getElementById('root'));
 ReactDOM.render(elem, document.getElementById('root'));
