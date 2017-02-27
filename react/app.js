18c5a99c7116020527d390be3dd47752754d524f
Crizzt <yuriy.evseenko@gmail.com>
2017-02-27 12:11:15 +0200

    react

--- react/app.js
+++ react/app.js
@@ -1,11 +1,38 @@
-var HelloMessage = React.createClass({
-	render: function(){
-		return <h3>Hello {this.props.message}</h3>
-		<h3>The World is {this.props.message}</h3>;
-	}
+var ToggleText = React.createClass({
+  getInitialState: function () {
+    return {
+      showDefault: true
+    }
+  },
+  
+  toggle: function (e) {
+    // Prevent following the link.
+    e.preventDefault();
+    
+    // Invert the chosen default.
+    // This will trigger an intelligent re-render of the component.
+    this.setState({ showDefault: !this.state.showDefault })
+  },
+  
+  render: function () {
+    // Default to the default message.
+    var message = this.props.default;
+    
+    // If toggled, show the alternate message.
+    if (!this.state.showDefault) {
+      message = this.props.alt;
+    }
+    
+    return (
+      <div>
+        <h1>Hello {message}!</h1>
+        <a href="" onClick={this.toggle}>Toggle</a>
+      </div>
+    );
+  }
 });
- 
+
 ReactDOM.render(
-  <HelloMessage message='World' />,
-  document.getElementById('root')
+	<ToggleText default="World" alt="Mars" />,
+	document.getElementById('root')
 );
