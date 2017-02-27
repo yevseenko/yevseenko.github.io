4b17e0d584bb023da8f11433733fcaf30ff13791
Crizzt <yuriy.evseenko@gmail.com>
2017-02-27 12:07:13 +0200

    react

--- react/app.js
+++ react/app.js
@@ -1,21 +1,11 @@
 var HelloMessage = React.createClass({
 	render: function(){
-		return <h3>Hello {this.props.message}</h3>;
-	}
-});
-
-var SecondMessage = React.createClass({
-	render: function() {
-		return <h3>The World is {this.props.message}</h3>;
+		return <h3>Hello {this.props.message}</h3>
+		<h3>The World is {this.props.message}</h3>;
 	}
 });
  
 ReactDOM.render(
   <HelloMessage message='World' />,
   document.getElementById('root')
-);
-
-ReactDOM.render(
-	<SecondMessage message='mine' />,
-	document.getElementById('root')
 );
