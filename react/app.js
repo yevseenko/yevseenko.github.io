c19f46e2403ca329ade361cd35343193096512aa
Crizzt <yuriy.evseenko@gmail.com>
2017-02-27 15:27:39 +0200

    react

--- react/app.js
+++ react/app.js
@@ -1,37 +1,58 @@
+class App extends React.Component {
+	render() {
+		return (
+			<div className='wrapper'>
+				<Menu />
+				<Clock />
+			</div>
+			);
+	}
+}
+
+class Menu extends React.Component {
+	render() {
+		return (
+			<div className='nav'>
+				<ul className='menu'>
+					<li>First menu item</li>
+					<li>Second menu item</li>
+					<li>Third menu item</li>
+				</ul>
+			</div>
+			);
+	}
+}
+
 class Clock extends React.Component {
 	constructor(props) {
     	super(props);
     	this.state = {date: new Date()};
   	}
-
   	componentDidMount() {
   		this.timerID = setInterval(
       		() => this.tick(),
       		1000
     	);
   	}
-
   	tick() {
     	this.setState({
       		date: new Date()
     	});
   	}
-
   	componentWillUnmount() {
   		clearInterval(this.timerID);
   	}
-
   	render() {
     	return (
       		<div>
-        		<h1>Hello, world!</h1>
-        		<h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+        		<p>It is {this.state.date.toLocaleTimeString()}.</p>
       		</div>
     	);
   	}
 }
 
+
 ReactDOM.render(
-  <Clock />,
+  <App />,
   document.getElementById('root')
 );
