98b8ddb347c0cc5f879303d9ccd941778d6efa05
crizzt <yuriy.evseenko@gmail.com>
2017-03-05 00:53:28 +0200

    'react'

--- react/app.js
+++ react/app.js
@@ -5,7 +5,7 @@ class App extends React.Component {
 				<Menu />
 				<Jumbotron />
 			</div>
-			);
+		);
 	}
 }
 
@@ -13,56 +13,55 @@ class Menu extends React.Component {
 	render() {
 		return (
 			<div className='nav'>
-					<a href='#'>First menu item</a>
-					<a href='#'>Second menu item</a>
-					<a href='#'>Third menu item</a>
-					<a href='#'>Fourth menu item</a>
-					<a href='#'>Fifth menu item</a>
-					<Clock />
+				<a href='#'>First menu item</a>
+				<a href='#'>Second menu item</a>
+				<a href='#'>Third menu item</a>
+				<a href='#'>Fourth menu item</a>
+				<a href='#'>Fifth menu item</a>
+				<Clock />
 			</div>
-			);
+		);
 	}
 }
 
 class Jumbotron extends React.Component {
 	render() {
 		return (
-			<div className='jumbotron'>
-			</div>
-			);
+			<div className='jumbotron'></div>
+		);
 	}
 }
 
 class Clock extends React.Component {
 	constructor(props) {
-    	super(props);
-    	this.state = {date: new Date()};
-  	}
-  	componentDidMount() {
-  		this.timerID = setInterval(
-      		() => this.tick(),
-      		1000
-    	);
-  	}
-  	tick() {
-    	this.setState({
-      		date: new Date()
-    	});
-  	}
-  	componentWillUnmount() {
-  		clearInterval(this.timerID);
-  	}
-  	render() {
-    	return (
-      		<div className='clock'>
-        		{this.state.date.toLocaleTimeString()}
-      		</div>
-    	);
-  	}
+		super(props);
+		this.state = { date: new Date() };
+	}
+	componentDidMount() {
+		this.timerID = setInterval(
+			() => this.tick(),
+			1000
+		);
+	}
+	tick() {
+		this.setState({
+			date: new Date()
+		});
+	}
+	componentWillUnmount() {
+		clearInterval(this.timerID);
+	}
+	render() {
+		return (
+			<div className='clock'>
+				{this.state.date.toLocaleTimeString()}
+			</div>
+		);
+	}
 }
 
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+	<App />,
+	document.getElementById('root')
 );
