5072e93597935acfed702c81ce70dfbec0f7af5b
Crizzt <yuriy.evseenko@gmail.com>
2017-03-20 19:21:38 +0200

    react

--- react/app.js
+++ react/app.js
@@ -32,10 +32,11 @@ class Toggle extends React.Component {
     super(props);
     this.state = {isToggleOn: true};
 
-    // This binding is necessary to make `this` work in the callbac
+    // This binding is necessary to make `this` work in the callback
+    this.handleClick = this.handleClick.bind(this);
   }
 
-  handleClick = () => {
+  handleClick() {
     this.setState(prevState => ({
       isToggleOn: !prevState.isToggleOn
     }));
