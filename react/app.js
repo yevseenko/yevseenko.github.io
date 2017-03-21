8592a29cbdad204cc77384d1171437c0312c161e
Crizzt <yuriy.evseenko@gmail.com>
2017-03-21 14:13:38 +0200

    react

--- react/app.js
+++ react/app.js
@@ -1,63 +1,13 @@
 class App extends React.Component {
-	render() {
-		return (
-			<div className='wrapper'>
-				<Menu />
-				<Jumbotron />
-			</div>
-		);
-	}
-}
-
-function ActionLink() {
-  function handleClick(e) {
-    e.preventDefault();
-    console.log('The link was clicked.');
-  }
-
-  return (
-    <a href="#" onClick={handleClick}>
-      Click me
-    </a>
-  );
-}
-
-
-
-class Toggle extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {isToggleOn: true};
-
-    // This binding is necessary to make `this` work in the callback
-    this.handleClick = this.handleClick.bind(this);
-  }
-
-  handleClick() {
-    this.setState(prevState => ({
-      isToggleOn: !prevState.isToggleOn
-    }));
-  }
-
-  render() {
-    return (
-      <button onClick={this.handleClick}>
-        {this.state.isToggleOn ? 'Click' : 'Setting'}
-      </button>
-    );
-  }
-}
-
-class Menu extends React.Component {
-	constructor() {
+		constructor() {
 		super();
 		this.state = {
 			menu: [
-				{id: 1, name: 'Home', href: '#'},
-				{id: 2, name: 'Second menu item', href: '#'},
-				{id: 3, name: 'Third menu item', href: '#'},
-				{id: 4, name: 'Fourth menu item', href: '#'},
-				{id: 5, name: 'About us', href: '#'}
+				{id: 1, name: 'ИКВ', href: '#'},
+				{id: 2, name: 'СЕС', href: '#'},
+				{id: 3, name: 'СОА', href: '#'},
+				{id: 4, name: 'ТЮА', href: '#'},
+				{id: 5, name: 'САС', href: '#'}
 			]
 		}
 
@@ -71,20 +21,23 @@ class Menu extends React.Component {
 
 	render() {
 		return (
+			<div className='wrapper'>
+				<Menu />
+				<Jumbotron />
+			</div>
+		);
+	}
+}
+
+class Menu extends React.Component {
+	render() {
+		return (
 			<div className='nav'>
 				{
 					this.state.menu.map((item) => <a key={item.id} href={item.href} onClick={this.handleClick}>{item.name}</a>)
 				}
 				<Search />
 			</div>
-		);
-	}
-}
-
-class Search extends React.Component {
-	render() {
-		return (
-				<input type='text' className='input-box'/>
 		);
 	}
 }
