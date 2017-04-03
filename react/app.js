670d3e73ff5c089c1e47f3f6cd381289e0addc20
Crizzt <yuriy.evseenko@gmail.com>
2017-04-03 15:10:01 +0300

    react

--- react/app.js
+++ react/app.js
@@ -37,8 +37,6 @@ class App extends React.Component {
 }
 
 const generateId = () => Math.floor(Math.random()*100000);
-const generateId = () => Math.floor(Math.random()*10000);
-
 const addItem = (list, item) => [...list, item];
 
 const Menu = (props) => {
