5cdd6c96384f9f3bc23918bee0bbbaaf638c526d
Crizzt <yuriy.evseenko@gmail.com>
2017-04-03 16:52:51 +0300

    pure

--- redux/app.js
+++ redux/app.js
@@ -1,151 +1,24 @@
-const state = {};
-
-state.todos = [];
-state.newTodoTitle;
-
-class StateStore {
-    constructor(initialState = {}, reducer) {
-        this.state = initialState;
-        this.listeners = [];
-        this.reducer = reducer;
-    }
-
-    subscribe(listener) {
-        this.listeners.push(listener);
-    }
-
-    dispatch(action) {
-        this.state = this.reducer(this.state, action);
-        this.listeners.forEach(listener => listener(this.state));
-    }
+window.onload = function() {
+    render();
 }
 
-const stateStore = new StateStore({
-    todos: [],
-    newTodoTitle: ''
-}, appReducer);
+const state = {
+    menu: [
+        { id: 0, name: 'menuItem', link: '#' },
+        { id: 1, name: 'menuItemSecond', link: '#' },
+        { id: 2, name: 'menuItemThird', link: '#' }
+    ]
+};
 
-stateStore.subscribe(render);
-
-let todoId = 0;
-
-function appReducer(state, action) {
-    switch (action.type) {
-        case 'SET_NEW_TODO_TITLE_VALUE':
-            return Object.assign({}, state, {
-                newTodoTitle: action.payload
-            });
-        case 'ADD_TODO':
-            return Object.assign({}, state, {
-                todos: state.todos.concat([Object.assign({ id: todoId++ }, action.payload)])
-            });
-        case 'COMPLETE_TODO':
-            return Object.assign({}, state, {
-                todos: state.todos.map(todo => {
-                    if (action.payload === todo.id) {
-                        todo.done = true;
-                    }
-                    return todo;
-                })
-            });
-    }
+function render() {
+    document.body.innerHTML = '<nav>' +
+        state.menu.map(function(item) {
+            return ('<a href="'+item.link+'">'+item.name+'</a>')
+        }).join(' | ') +
+        '</nav>';
 }
 
-function render(state) {
-    document.body.innerHTML = renderToString(state);
-}
-
-function renderToString(state) {
-    return `
-		<input type="text" value="${state.newTodoTitle || ''}"></input>
-
-		<ul>
-			${renderTodos(state.todos)}
-		</ul>
-
-		<ul>
-		</ul>
-
-	`;
-}
-
-function renderTodos(todos) {
-    return todos.map(todo => {
-            return `<li 
-					class="todo-item${todo.done ? ' -completed' : ''}"
-					data-id="${todo.id}"
-					>
-				
-				${todo.title}
-
-				</li>
-				`
-        })
-        .join('\n');
-}
-
-render(state);
-
-document.body.addEventListener('keypress', e => {
-    const input = document.querySelector('input');
-
-    if (e.keyCode === 13) {
-        stateStore.dispatch({
-            type: 'ADD_TODO',
-            payload: {
-                title: input.value,
-                done: false
-            }
-        });
-
-        stateStore.dispatch({
-            type: 'SET_NEW_TODO_TITLE_VALUE',
-            payload: ''
-        });
-
-        return;
-    }
-
-    stateStore.dispatch({
-        type: 'SET_NEW_TODO_TITLE_VALUE',
-        payload: input.value + e.key
-    });
-
-    makeMyLifeAwesome();
+document.addEventListener('click', function(e) {
+    state.menu.push({ name: 'newLink' });
+    render();
 });
-
-document.body.addEventListener('click', e => {
-    if (e.target.classList.contains('todo-item')) {
-        stateStore.dispatch({
-            type: 'COMPLETE_TODO',
-            payload: +e.target.dataset.id
-        })
-    }
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-function makeMyLifeAwesome() {
-    setTimeout(() => {
-        document.querySelector('input').focus();
-    }, 0);
-}
