d95b410ab7b7042c3f1082dcffd9378d2de2f097
Crizzt <yuriy.evseenko@gmail.com>
2017-04-06 19:37:49 +0300

    react

---
+++ react/test.js
@@ -0,0 +1,39 @@
+class App extends React.Component {
+	state = {
+		menu: []
+	}
+
+	componentDidMount() {
+		loadState()
+    		.then(menu => this.setState({menu}));
+  }
+
+	render() {
+		return (
+			<div className='container'>
+				<Home />
+			</div>
+		);
+	}
+}
+
+const loadState = () => {
+    return {
+			menu: [
+				{ key: 1, name: 'Home', link: '#home' },
+				{ key: 2, name: 'awesomeGame', link: '#app' },
+				{ key: 3, name: 'About', link: '#about' }
+			],
+}
+
+const Home = (props) => {
+	return (
+		<div className='well well-sm'>Homepage of awesome game.. =)</div>
+	)
+}
+
+
+ReactDOM.render(
+	<App />,
+	document.getElementById('root')
+);
