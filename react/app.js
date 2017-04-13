4daad9f94b8186c35f3d95f1e086d0fee40a44d6
void <yuriy.evseenko@gmail.com>
2017-04-13 11:44:48 +0300

    react

--- react/app.js
+++ react/app.js
@@ -3,25 +3,25 @@ class App extends React.Component {
 		super(props);
 		this.state = {
 			control: [
-				{ id: 1, name: 'Damage up', used: false, message: ''},
-				{ id: 2, name: 'Armour up', used: false, message: ''},
-				{ id: 3, name: 'Restore HP', used: true, message: ''},
-				{ id: 4, name: 'Awesome skill -50hp', used: false, message: ''},
-				{ id: 5, name: 'Debuff enemy armour', used: true, message: ''},
-				{ id: 6, name: 'Debuff enemy damage', used: false, message: ''}
+				{ id: 1, name: 'Damage up', used: false, message: '' },
+				{ id: 2, name: 'Armour up', used: false, message: '' },
+				{ id: 3, name: 'Restore HP', used: true, message: '' },
+				{ id: 4, name: 'Awesome skill -50hp', used: false, message: '' },
+				{ id: 5, name: 'Debuff enemy armour', used: true, message: '' },
+				{ id: 6, name: 'Debuff enemy damage', used: false, message: '' }
 			],
 
 			player: {
 				name: 'myPlayer',
 				hitpoints: 150,
-        currentHp: 150,
+				currentHp: 150,
 				damage: 50
 			},
 
 			enemy: {
 				name: 'myEnemy',
 				hitpoints: 150,
-        currentHp: 150,
+				currentHp: 150,
 				damage: 35
 			},
 
@@ -32,26 +32,26 @@ class App extends React.Component {
 	}
 
 	handleAction(evt) {
-			const updatedPlayer = {
-				name: this.state.player.name,
-				hitpoints: this.state.player.hitpoints,
-				currentHp: this.state.player.currentHp,
-				damage: this.state.player.damage + 20
-			}
-			if (evt.target.innerHTML === 'Damage up') {
-				this.setState({ msg: 'Damage is up', player: updatedPlayer });
-			}
-			console.log(evt.target.id);
+		const updatedPlayer = {
+			name: this.state.player.name,
+			hitpoints: this.state.player.hitpoints,
+			currentHp: this.state.player.currentHp,
+			damage: this.state.player.damage + 20
+		}
+		if (evt.target.innerHTML === 'Damage up') {
+			this.setState({ msg: 'Damage is up', player: updatedPlayer });
+		}
+		console.log(evt.target.id);
 	}
 
 	render() {
 		return (
 			<div className='container'>
-				<Game player={this.state.player} 
-					enemy={this.state.enemy} 
-					handleAction={this.handleAction} 
+				<Game player={this.state.player}
+					enemy={this.state.enemy}
+					handleAction={this.handleAction}
 					control={this.state.control}
-					msg={this.state.msg}/>
+					msg={this.state.msg} />
 			</div>
 		);
 	}
@@ -60,10 +60,10 @@ class App extends React.Component {
 const Game = (props) => {
 	return (
 		<div>
-			<Control handleAction={props.handleAction} control={props.control}/>
+			<Control handleAction={props.handleAction} control={props.control} />
 			<div className='row'>
 				<Player player={props.player} />
-				<Panel msg={props.msg}/>
+				<Panel msg={props.msg} />
 				<Enemy enemy={props.enemy} />
 			</div>
 		</div>
@@ -76,10 +76,10 @@ const Player = (props) => {
 			<div className='well well-sm'>
 				Player
       	<div><b>Name:</b> {props.player.name}</div>
-      	<div><b>Hitpoints:</b> {props.player.currentHp}/{props.player.hitpoints}</div>
-      	<div><b>Damage:</b> {props.player.damage}</div>
-      </div>
-    </div>
+				<div><b>Hitpoints:</b> {props.player.currentHp}/{props.player.hitpoints}</div>
+				<div><b>Damage:</b> {props.player.damage}</div>
+			</div>
+		</div>
 	);
 }
 
@@ -99,29 +99,29 @@ const Control = (props) => {
 		<div className='row'>
 			<div className='col-xs-12'>
 				<div className='well well-sm'>
-        	<div className='btn-group btn-group-sm'>
-          	<button className='btn'><b>Control Panel:</b></button>
-				  	{
-				  		props.control.map((item) => {
-				  			if (item.used === true) {
-				  				return (<button className='btn btn-primary' 
-				  						  			key={item.id}
-				  						  			disabled='disabled'
-				  						  			onClick={props.handleAction}>
-				  						  				{item.name}
-				  						  			</button>
-				  					)
-				  			} else {
-				  				return (<button className='btn btn-primary' 
-				  						  			key={item.id}
-				  						  			onClick={props.handleAction}>
-				  						  				{item.name}
-				  						  			</button>				  					
-				  					)
-				  			}
-				  		})
-				  	}
-        	</div>
+					<div className='btn-group btn-group-sm'>
+						<button className='btn'><b>Control Panel:</b></button>
+						{
+							props.control.map((item) => {
+								if (item.used === true) {
+									return (<button className='btn btn-primary'
+										key={item.id}
+										disabled='disabled'
+										onClick={props.handleAction}>
+										{item.name}
+									</button>
+									)
+								} else {
+									return (<button className='btn btn-primary'
+										key={item.id}
+										onClick={props.handleAction}>
+										{item.name}
+									</button>
+									)
+								}
+							})
+						}
+					</div>
 				</div>
 			</div>
 		</div>
@@ -134,10 +134,10 @@ const Enemy = (props) => {
 			<div className='well well-sm'>
 				Enemy
       	<div><b>Name:</b> {props.enemy.name}</div>
-      	<div><b>Hitpoints:</b> {props.enemy.currentHp}/{props.enemy.hitpoints}</div>
-      	<div><b>Damage:</b> {props.enemy.damage}</div>
-      </div>
-    </div>
+				<div><b>Hitpoints:</b> {props.enemy.currentHp}/{props.enemy.hitpoints}</div>
+				<div><b>Damage:</b> {props.enemy.damage}</div>
+			</div>
+		</div>
 	);
 }
 
