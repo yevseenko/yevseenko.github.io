73ef5b6652a77c60d964718a52603c80f99562e5
yuriy.evseenko@gmail.com <yuriy.evseenko@gmail.com>
2017-05-12 16:39:22 +0300

    basic task f

--- static/js/test.js
+++ static/js/test.js
@@ -8,7 +8,7 @@
         cHours = date.getHours(),
         cMinutes = date.getMinutes(),
         cSeconds = date.getSeconds();
-    
+
     function addZero(num) {
         if (num < 10) {
             return '0' + num;
@@ -25,7 +25,7 @@
     var cDate = date.getDate(),
         cMonth = date.getMonth(),
         cYear = date.getFullYear();
-    
+
     function addZero(num) {
         if (num < 10) {
             return '0' + num;
@@ -34,4 +34,34 @@
     }
 
     document.getElementById('basic-two').innerHTML = addZero(cDate) + '.' + addZero(cMonth) + '.' + cYear;
+}());
+
+(function () {
+    var a = 5,
+        b = 6,
+        c = 7;
+
+    var p = (a + b + c) / 2;
+
+    var area = Math.sqrt(p * (p - a) * (p - b) * (p - c));
+
+    document.getElementById('basic-three').innerHTML = area;
+}());
+
+
+
+(function () {
+    var str = 'Hello World ';
+
+    setInterval(function () {
+        var sub = str.slice(-1);
+        str = sub.concat(str.slice(0, str.length - 1));
+
+        document.getElementById('basic-four').innerText = str;
+    }, 300);
+
+}());
+
+(function () {
+
 }());
