da9dc0de34e03bdb8c01baa24c8bedc15b1820b1
yevseenko <yuriy.evseenko@gmail.com>
2017-08-31 22:18:10 +0300

    'src'

--- react/con.cs
+++
@@ -1,6 +0,0 @@
-using System;
-class MainClass{
-  public static void Main(){
-    Console.WriteLine("Hello world");
-  }
-}
--- static/crc-src/js/char.js
+++ static/crc-src/js/char.js
@@ -22,4 +22,75 @@ jQuery(function($) {
   });
 });
 
-console.log('Page loaded');
+var MONTHS = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+var config = {
+    type: 'line',
+    data: {
+        labels: ["January", "February", "March", "April", "May", "June", "July"],
+        datasets: [{
+            label: "До чипа",
+            backgroundColor: window.chartColors.red,
+            borderColor: window.chartColors.orange,
+            data: [
+                10,
+                20,
+                30,
+                40,
+                65,
+                40,
+                30
+            ],
+            fill: -1,
+        }, {
+            label: "После чипа",
+            fill: -8,
+            backgroundColor: window.chartColors.blue,
+            borderColor: window.chartColors.green,
+            data: [
+                20,
+                30,
+                40,
+                50,
+                85,
+                50,
+                40
+            ],
+        }]
+    },
+    options: {
+        responsive: true,
+        title:{
+            display:true,
+            text:'Л.С. Автомобиля'
+        },
+        tooltips: {
+            mode: 'index',
+            intersect: false,
+        },
+        hover: {
+            mode: 'nearest',
+            intersect: true
+        },
+        scales: {
+            xAxes: [{
+                display: true,
+                scaleLabel: {
+                    display: true,
+                    labelString: 'Month'
+                }
+            }],
+            yAxes: [{
+                display: true,
+                scaleLabel: {
+                    display: true,
+                    labelString: 'Value'
+                }
+            }]
+        }
+    }
+};
+
+window.onload = function() {
+    var ctx = document.getElementById("chart-0").getContext("2d");
+    window.myLine = new Chart(ctx, config);
+};
