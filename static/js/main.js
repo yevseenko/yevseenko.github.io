1b7eb00d809b77ac33a932e7d8e81400522aea1d
yuriy.evseenko@gmail.com <yuriy.evseenko@gmail.com>
2019-02-25 15:33:49 +0200

    clockwerk time"

--- static/js/main.js
+++ static/js/main.js
@@ -1,14 +1,11 @@
-window.onload = function() {
+window.onload = function () {
 	$dclock = document.getElementById('dclock');
 	digitalClock();
 }
 
-var dayOfWeek = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"],
-month = ["January","February","March","April","May","June","July","August","September","October","November","December"];
-
 function digitalClock() {
 	$dclock.innerHTML = formatedString();
-   	setTimeout(digitalClock, 1000);
+	setTimeout(digitalClock, 1000);
 };
 
 function addZero(n) {
@@ -20,9 +17,20 @@ function addZero(n) {
 };
 
 function formatedString() {
-	var start = Date.parse("Mar 31, 2015");
-	var date = Date.now();
-	var result = date - start;
+	var creationDate = Date.parse("Mar 31, 2015");
+	var currentDate = Date.now();
+	var lifetime = currentDate - creationDate;
 
-	return result;
+	var totalSeconds = lifetime / 1000;
+	var years = parse(totalSeconds / 31536000);
+	var days = parse((totalSeconds - years * 31536000) / 86400);
+	var hours = parse(totalSeconds / 3600 - years * 8760 - days * 24);
+	var minutes = parse(totalSeconds / 60 - years * 525600 - days * 1440 - hours * 60);
+	var seconds = parse(totalSeconds - years * 31536000 - days * 86400 - hours * 3600 - minutes * 60);
+
+	return "There is still nothing " + years + " years " + days + " days " + addZero(hours) + " hours " + addZero(minutes) + " minutes " + addZero(seconds) + " seconds.";
 };
+
+function parse(value) {
+	return Number.parseInt(value);
+}
